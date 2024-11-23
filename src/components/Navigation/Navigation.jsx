c31e3f4c4864315a045155d36fc11e9f8c3e984b
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-11-23 19:19:05 +0100

    movies

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import s from "./Navigation.module.css";
 import clsx from "clsx";
 
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import { Field, Form, Formik } from "formik";
+import { useState } from "react";
+import MovieList from "../../components/MovieList/MovieList";
 import { Outlet } from "react-router-dom";
 
 const MoviesPage = () => {
+  const [movies, setMovies] = useState([]);
+
+  const [query, setQuery] = useState("");
+
+  const filteredData = query
+    ? movies.filter((movie) =>
+        movie.title.toLowerCase().includes(query.toLowerCase())
+      )
+    : movies;
+
+  const handleSubmit = (values) => {
+    setQuery(values.query); // Оновлюємо запит
+  };
+  const initialValues = {
+    query: "",
+  };
   return (
     <div>
+      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+        <Form>
+          <Field name="query" />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+      <MovieList movies={filteredData} />
       <Outlet />
     </div>
   );
