e30d07bb884d90494c98d74b50640571217666cf
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-11-23 11:42:42 +0100

    path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,12 +12,12 @@ const App = () => {
   return (
     <div>
       <Navigation />
-         <Routes>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />}>
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="/movies/:movieId/cast" element={<MovieCast />} />
-            <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+          <Route path=":movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
           </Route>
         </Route>
         <Route path="*" element={<NotFoundPage />} />
