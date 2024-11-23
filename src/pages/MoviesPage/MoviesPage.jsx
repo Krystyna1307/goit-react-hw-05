b7ea79823d78674432e5e4eb0b908ce778c18cd2
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-11-23 19:43:18 +0100

    form

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,34 +1,48 @@
 import { Field, Form, Formik } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MovieList from "../../components/MovieList/MovieList";
 import { Outlet } from "react-router-dom";
+import { searchMovies } from "../../services/api";
 
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
-
   const [query, setQuery] = useState("");
 
-  const filteredData = query
-    ? movies.filter((movie) =>
-        movie.title.toLowerCase().includes(query.toLowerCase())
-      )
-    : movies;
+  useEffect(() => {
+    const getData = async () => {
+      const data = await searchMovies(query);
+      setMovies(data);
+    };
+    getData();
+  }, [query]);
 
   const handleSubmit = (values) => {
-    setQuery(values.query); // Оновлюємо запит
+    if (values.query.trim() !== "" && values.query !== query) {
+      setQuery(values.query);
+    }
   };
+
   const initialValues = {
     query: "",
   };
+
   return (
     <div>
-      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-        <Form>
-          <Field name="query" />
-          <button type="submit">Search</button>
-        </Form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, { setSubmitting }) => {
+          handleSubmit(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ handleChange, values }) => (
+          <Form>
+            <Field name="query" value={values.query} onChange={handleChange} />
+            <button type="submit">Search</button>
+          </Form>
+        )}
       </Formik>
-      <MovieList movies={filteredData} />
+      <MovieList movies={movies} />
       <Outlet />
     </div>
   );
