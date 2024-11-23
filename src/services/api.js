1da5cf558f894bf08b7092e04775a455fbc283c2
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-11-23 16:37:47 +0100

    api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTI2YzRkODc3NzJlOGZkMDk4ZjE1NGJmZDgzMDYyZiIsIm5iZiI6MTczMjI4OTAwMS40MzI1MTI1LCJzdWIiOiI2NzQwNTJhM2RhZTJlNmE5MzgyNTVjZDgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.NYvUmnnujAywcRI48gBdKfz-BHUsS9GbmMNlxr7Y-28";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const BASE_URL = "https://api.themoviedb.org/3";
 
